8d88f6a2f9249657adfe50900233fabb5333dced
Kevin-W95 <kevin.wu@ulead.ac.cr>
2026-04-12 18:58:44 -0600

    Finish

--- Trabajos en Clase/Trabajo_en_Clase_8/FileConverter.py
+++ Trabajos en Clase/Trabajo_en_Clase_8/FileConverter.py
@@ -1,23 +1,25 @@
 import json
 import csv
+import os
 
 class FileConverter:
     _instance = None
 
     def __new__(cls):
-        #Garantiza que solo exista una única instancia 
         if cls._instance is None:
             cls._instance = super(FileConverter, cls).__new__(cls)
+            cls._instance.base_dir = os.path.dirname(os.path.abspath(__file__))
         return cls._instance
 
     def convertFromJson(self, jsonFileStr):
-        """Lee un archivo JSON y lo retorna como diccionario"""
-        with open(jsonFileStr, 'r', encoding='utf-8') as file:
+        ruta_completa = os.path.join(self.base_dir, jsonFileStr)
+        
+        with open(ruta_completa, 'r', encoding='utf-8') as file:
             return json.load(file)
 
     def convertFromCSV(self, csvFileStr):
-        """Lee un archivo CSV y lo retorna como diccionario"""
-        with open(csvFileStr, mode='r', encoding='utf-8') as file:
-            #DictReader convierte cada fila en un diccionario [cite: 14]
+        ruta_completa = os.path.join(self.base_dir, csvFileStr)
+        
+        with open(ruta_completa, mode='r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             return [row for row in reader]
--- Trabajos en Clase/Trabajo_en_Clase_8/Prueba.py
+++ Trabajos en Clase/Trabajo_en_Clase_8/Prueba.py
@@ -1,21 +1,18 @@
-from FileConverter import FileConverter 
+from FileConverter import FileConverter
 
 converter = FileConverter()
 
-#Método para JSON
 try:
-    datos_json = converter.convertFromJson("Trabajos en Clase/Trabajo_en_Clase_8/ejemplo.json")
+    datos_json = converter.convertFromJson("ejemplo.json")
     print("Contenido JSON:", datos_json)
 except FileNotFoundError:
-    print("Error: El archivo JSON no fue encontrado.")
+    print("Error: No se encontró el archivo JSON.")
 
-#Método para CSV
 try:
-    datos_csv = converter.convertFromCSV("Trabajos en Clase/Trabajo_en_Clase_8/ejemplo.csv")
+    datos_csv = converter.convertFromCSV("ejemplo.csv")
     print("Contenido CSV:", datos_csv)
 except FileNotFoundError:
-    print("Error: El archivo CSV no fue encontrado.")
+    print("Error: No se encontró el archivo CSV.")
 
-#Verificación del Patrón Singleton
 otra_instancia = FileConverter()
 print(f"¿Son la misma instancia?: {converter is otra_instancia}")
